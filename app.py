cf7afe95c74342b13dd87a54529a32b98d9c1e92
waleplanet <walexrealest@gmail.com>
2018-07-19 20:56:18 +0100

    resize image

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def generate_images():
     incoming = json.loads(request.data.decode())
 
     wsq_incoming = base64.b64decode(incoming['wsq_image'])
-    png_incoming = base64.b64decode(incoming['png_image'])
+    png_incoming = incoming['png_image']
     # incoming = incoming['image']
 
     # name = fake.name()
@@ -141,7 +141,7 @@ def generate_images():
         
         
     make_wsq(wsq_incoming,last_name)
-    make_png(png_incoming,last_name)
+    # make_png(png_incoming,last_name)
     make_bmp(png_incoming,last_name)
     return Response(status=200)
 
